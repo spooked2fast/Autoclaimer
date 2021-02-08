b852b4012f68bd5ba79388682de770e3910d9970
amen <parasjhaisashitdev@protonmail.com>
2021-02-07 23:38:57 -0800

    Fixed session for grabbing token

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args){
         FileIO fileIO = new FileIO();
-        System.out.println(fileIO.getDataDirectoryPath());
         YamlParser yamlParser = new YamlParser();
         Settings settings = yamlParser.parseYaml("Settings.yaml");
-        System.out.println(settings.getAuthThreadCount());
         XboxLiveAuth xboxLiveAuth = new XboxLiveAuth();
         xboxLiveAuth.login("allxbl1337@outlook.com","#$tayS4fer");
     }
